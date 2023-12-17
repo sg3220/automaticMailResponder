0327fd62b90341023a76237af993b97611361372
Siddharth Ghosh <email>
2023-12-17 19:24:04 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const HTTP = require('http');
 const { google } = require('googleapis');
 const { extractMessageID } = require('./myMethods.js');
 
@@ -5,6 +6,7 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const PORT = process.env.PORT_NUMBER;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
@@ -21,11 +23,23 @@ const gmailInstance = google.gmail({ version: 'v1', auth: oAuth2Client });
 const runTask = () => {
   extractMessageID(gmailInstance, oAuth2Client);
 };
+let myServer;
+const ServerCreation = () => {
+  myServer = HTTP.createServer((Req, Res) => {
+    Res.writeHead(200, 'OK', { cookieToken: 'NULL' });
+    Res.end();
+  });
+};
+
+myServer.listen(PORT, () => {
+  console.log(`Server Working On ${PORT}`);
+});
 
 console.log('#0:');
 runTask();
 
 setInterval(() => {
-  console.log('Again: ');
   runTask();
-}, 45 * 1000);
+}, 90 * 1000);
+
+ServerCreation();
